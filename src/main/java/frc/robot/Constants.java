efee1e94638df464b0da93ca093b7e5048295873
AimeeAndrews <aimeea1218@gmail.com>
2024-01-20 10:31:48 -0800

    Started Climber Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,10 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class ClimberSubsystem {
+    public static final int kClimberMotorPort = 0;
+  }
+
+
 }
