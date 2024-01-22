5340e3088e22723fb95102a3ab3010ca767e4f36
JHstrutton2000 <49569114+JHstrutton2000@users.noreply.github.com>
2024-01-21 17:05:23 -0800

    Made a multiThread Manager

---
+++ src/main/java/frc/robot/threads/exampleRunnableThread.java
@@ -0,0 +1,39 @@
+package frc.robot.threads;
+
+import java.nio.ByteBuffer;
+import frc.robot.extensions.RunnableThread;
+
+class testThread implements Runnable{    
+    RunnableThread parent;
+    ByteBuffer buff;
+
+    testThread(RunnableThread parentRunnableThread){
+        parent = parentRunnableThread;
+        parent.packBuffer(buff);
+    }
+
+    @Override
+    public void run(){
+        int i = 0;
+        while(true){
+
+            if(i%20 == 0){
+                ByteBuffer buff = ByteBuffer.allocate(Long.BYTES).putDouble((double)i);
+                parent.putBuffer(buff);
+            }
+
+            if(i >= 100){
+                i = 0;
+            }
+            i++;
+        }
+    }
+}
+
+public class exampleRunnableThread extends RunnableThread{
+    public exampleRunnableThread(){
+        putBuffer(ByteBuffer.allocate(Long.BYTES).putDouble(0.001));
+
+        setMethod(new testThread(this));
+    }
+}
