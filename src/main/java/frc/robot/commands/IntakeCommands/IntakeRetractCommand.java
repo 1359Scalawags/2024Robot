be77659c582411328633ee77d73c511596109ff0
AimeeAndrews <aimeea1218@gmail.com>
2024-02-26 20:17:38 -0800

    Speed tuning

--- src/main/java/frc/robot/commands/IntakeCommands/IntakeRetractCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeRetractCommand.java
@@ -43,6 +43,6 @@ public class IntakeRetractCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return m_IntakeSubsystem.isUp();
   }
 }
