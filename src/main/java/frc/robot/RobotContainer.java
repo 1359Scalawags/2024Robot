dc9ab8b8010f04ef62a8fc9a3d33b4dfe89b2972
NASA-12-1962 <knoxbeaufort@protonmail.com>
2024-03-08 07:09:02 -0800

    commented unused auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,7 +108,7 @@ SendableChooser<Command> autoChooser;
     autoChooser.addOption("Basic Pos 3", getAutonomousCommand("Basic Pos 3"));
     autoChooser.addOption("Basic Pos 1", getAutonomousCommand("Basic Pos 1"));
     //autoChooser.addOption("MoveOnly", getAutonomousCommand("MoveOnly"));
-    autoChooser.addOption("MoveAndShoot", getAutonomousCommand("MoveAndShoot"));
+    //autoChooser.addOption("MoveAndShoot", getAutonomousCommand("MoveAndShoot"));
     //autoChooser.addOption("Test Auto Two", getAutonomousCommand("Test Auto Two"));
     //autoChooser.addOption("Test Auto", getAutonomousCommand("Test Auto"));
     //autoChooser.addOption("Two Note Auto pos 2", getAutonomousCommand("Two Note Auto pos 2"));
