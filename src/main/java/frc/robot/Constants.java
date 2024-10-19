53bce270714d7ef2364fa8a4a6eff4f37207eda1
NASA-12-1962 <knoxbeaufort@protonmail.com>
2024-10-19 15:40:20 -0700

    Changed driver speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,7 +39,7 @@ public final class Constants {
     //public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
     //public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
-    public static final double kMaxRobotSpeed = 10.50;
+    public static final double kMaxRobotSpeed = 12.64;
     public static final double kDriveConversionFactor = 4.0;
     public static final double kAngleConversionFactor = 21.4285714286;
     public static final double kDriveGearRatio = 6.75;
